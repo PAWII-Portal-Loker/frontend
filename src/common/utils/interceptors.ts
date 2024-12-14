912489d3a153b03db0426deb34348f44de4e1f96
Rendy Pratama <rendypratama752@gmail.com>
2024-12-14 15:51:03 +0700

    refactor: interceptors

--- src/common/utils/interceptors.ts
+++ src/common/utils/interceptors.ts
@@ -1,27 +1,61 @@
-import { AxiosInstance } from "axios";
+import {
+  AxiosInstance,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from "axios";
 import { DeviceUUID } from "device-uuid";
+
+const authReqInterceptors = (config: InternalAxiosRequestConfig) => {
+  const accessToken = localStorage.getItem("access_token");
+  const refreshToken = localStorage.getItem("refresh_token");
+  const userId = localStorage.getItem("user_id");
+
+  if (accessToken) {
+    config.headers["x-access-token"] = accessToken;
+  }
+  if (refreshToken) {
+    config.headers["x-refresh-token"] = refreshToken;
+  }
+  if (userId) {
+    config.headers["x-user-id"] = userId;
+  }
+
+  return config;
+};
+
+const authResInterceptors = (response: AxiosResponse) => {
+  const accessToken = response.headers["x-access-token"];
+  const refreshToken = response.headers["x-refresh-token"];
+  const user_id = response.headers["x-user-id"];
+
+  if (accessToken) {
+    localStorage.setItem("access_token", accessToken);
+  }
+  if (refreshToken) {
+    localStorage.setItem("refresh_token", refreshToken);
+  }
+  if (user_id) {
+    localStorage.setItem("user_id", user_id);
+  }
+
+  return response;
+};
+
+const deviceIdInterceptors = (config: InternalAxiosRequestConfig) => {
+  const deviceId = new DeviceUUID().get();
+
+  if (deviceId) {
+    config.headers["x-device-id"] = deviceId;
+  }
+
+  return config;
+};
 
 export const applyInterceptors = (api: AxiosInstance) => {
   api.interceptors.request.use(
     (config) => {
-      const accessToken = localStorage.getItem("access_token");
-      const refreshToken = localStorage.getItem("refresh_token");
-      const userId = localStorage.getItem("user_id");
-      const deviceId = new DeviceUUID().get();
-
-      if (accessToken) {
-        config.headers["x-access-token"] = accessToken;
-      }
-      if (refreshToken) {
-        config.headers["x-refresh-token"] = refreshToken;
-      }
-      if (userId) {
-        config.headers["x-user-id"] = userId;
-      }
-      if (deviceId) {
-        config.headers["x-device-id"] = deviceId;
-      }
-
+      authReqInterceptors(config);
+      deviceIdInterceptors(config);
       return config;
     },
     (error) => {
@@ -30,20 +64,7 @@ export const applyInterceptors = (api: AxiosInstance) => {
   );
 
   api.interceptors.response.use((response) => {
-    const accessToken = response.headers["x-access-token"];
-    const refreshToken = response.headers["x-refresh-token"];
-    const user_id = response.headers["x-user-id"];
-
-    if (accessToken) {
-      localStorage.setItem("access_token", accessToken);
-    }
-    if (refreshToken) {
-      localStorage.setItem("refresh_token", refreshToken);
-    }
-    if (user_id) {
-      localStorage.setItem("user_id", user_id);
-    }
-
+    authResInterceptors(response);
     return response;
   });
 };
